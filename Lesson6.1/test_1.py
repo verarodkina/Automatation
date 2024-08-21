72a50b06439db4f7c734a2d7eb5798b86ceb3222
verarodkina <verarodkina1999@yandex.ru>
2024-08-22 00:57:36 +0300

    доработка КР

--- Lesson6.1/test_1.py
+++ Lesson6.1/test_1.py
@@ -3,43 +3,32 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from conftest import *
 from data import *
-from time import sleep
+from time import sleep 
 
 def test_data_types_form(chrome_browser):
     chrome_browser.get(url_1)
-    form_data = {
-    "first-name": first_name,
-    "last-name": last_name,
-    "address": address,
-    "e-mail": email,
-    "phone": phone,
-    "zip-code": zip_code,
-    "city": city,
-    "country": country,
-    "job_position": job_position,
-    "company": company
-    }
+    chrome_browser.find_element(By.NAME, "first-name").send_keys(first_name)
+    chrome_browser.find_element(By.NAME, "last-name").send_keys(last_name)
+    chrome_browser.find_element(By.NAME, "address").send_keys(address)
+    chrome_browser.find_element(By.NAME, "e-mail").send_keys(email)
+    chrome_browser.find_element(By.NAME, "phone").send_keys(phone)
+    chrome_browser.find_element(By.NAME, "zip-code").send_keys(zip_code)
+    chrome_browser.find_element(By.NAME, "city").send_keys(city)
+    chrome_browser.find_element(By.NAME, "country").send_keys(country)
+    chrome_browser.find_element(By.NAME, "job-position").send_keys(job_position)
+    chrome_browser.find_element(By.NAME, "company").send_keys(company)
 
-    for field_name, value in form_data.items():
-        chrome_browser.find_element(By.NAME, field_name).send_keys(value)
-
-    WebDriverWait(chrome_browser, 40, 0.1).until(
-        EC.element_to_be_clickable((By.TAG_NAME, "button"))).click()
+    WebDriverWait(chrome_browser, 40, 0.1).until(EC.element_to_be_clickable((By.TAG_NAME, "button"))).click()
     sleep(2)
 
-    field_classes = {
-        "zip-code": "danger",
-        "first-name":"success",
-        "last-name":"success",
-        "address":"success",
-        "e-mail":"success",
-        "phone":"success",
-        "city":"success",
-        "country":"success",
-        "job-position":"success",
-        "company":"success"
-    }
+    assert "danger" in chrome_browser.find_element(By.ID, "zip-code").get_attribute("class")
+    assert "success" in chrome_browser.find_element(By.ID, "first-name").get_attribute("class")
+    assert "success" in chrome_browser.find_element(By.ID, "last-name").get_attribute("class")
+    assert "success" in chrome_browser.find_element(By.ID, "address").get_attribute("class")
+    assert "success" in chrome_browser.find_element(By.ID, "e-mail").get_attribute("class")
+    assert "success" in chrome_browser.find_element(By.ID, "phone").get_attribute("class")
+    assert "success" in chrome_browser.find_element(By.ID, "city").get_attribute("class")
+    assert "success" in chrome_browser.find_element(By.ID, "country").get_attribute("class")
+    assert "success" in chrome_browser.find_element(By.ID, "job-position").get_attribute("class")
+    assert "success" in chrome_browser.find_element(By.ID, "company").get_attribute("class")
 
-    for field_id, class_name in field_classes.items():
-        assert class_name in chrome_browser.find_element(
-            By.ID, field_id).get_attribute("class")
--- Lesson6.1/test_3.py
+++ Lesson6.1/test_3.py
@@ -3,13 +3,13 @@ from data import *
 
 def test_shop(chrome_browser):
     chrome_browser.get(url_3)
-    chrome_browser.find_element(By.ID, "user-name").send_keys("standart_user")
+    chrome_browser.find_element(By.ID, "user-name").send_keys("standard_user")
     chrome_browser.find_element(By.ID, "password").send_keys("secret_sauce")
     chrome_browser.find_element(By.ID, "login-button").click()
     chrome_browser.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
     chrome_browser.find_element(By.ID, "add-to-cart-sauce-labs-bolt-t-shirt").click()
     chrome_browser.find_element(By.ID, "add-to-cart-sauce-labs-onesie").click()
-    chrome_browser.find_element(By.ID, "shoping_cart_contrainer").click()
+    chrome_browser.find_element(By.ID, "shopping_cart_container").click()
     chrome_browser.find_element(By.ID, "checkout").click()
     chrome_browser.find_element(By.ID, "first-name").send_keys("Алина")
     chrome_browser.find_element(By.ID, "last-name").send_keys("Петрова")
